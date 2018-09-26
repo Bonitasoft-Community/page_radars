3ec98c74b84a99b43eb9c5377d0e50802763754c
pierre-yves-monnet <pierre-yves.monnet@bonitasoft.com>
2018-09-26 19:03:51 -0400

    Separate thread workers and thread connector

---
+++ resources/ctrlmonitor.js
@@ -0,0 +1,132 @@
+'use strict';
+/**
+ *
+ */
+
+(function() {
+
+
+var appCommand = angular.module('ctrlmonitor', ['googlechart', 'ui.bootstrap','ngSanitize', 'ngModal', 'ngMaterial']);
+
+
+/* Material : for the autocomplete
+ * need
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js"></script>
+
+  <!-- Angular Material Library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js">
+ */
+
+
+
+// --------------------------------------------------------------------------
+//
+// Controler Ping
+//
+// --------------------------------------------------------------------------
+
+// Ping the server
+appCommand.controller('CtrlControler',
+	function ( $http, $scope,$sce,$filter ) {
+
+	this.pingdate='';
+	this.pinginfo='';
+	this.listevents='';
+	this.inprogress=false;
+
+	this.refresh = function()
+	{
+
+		var self=this;
+		self.inprogress=true;
+		// 7.6 : the server force a cache on all URL, so to bypass the cache, then create a different URL
+		var d = new Date();
+
+		$http.get( '?page=custompage_workers&action=refresh&t='+d.getTime() )
+				.success( function ( jsonResult ) {
+						console.log("history",jsonResult);
+						self.radars 		= jsonResult.collect.radars;
+						self.timecollectms	= jsonResult.collect.timecollectms;
+						self.datecollectms	= jsonResult.collect.datecollectms;
+						
+						self.listevents		= jsonResult.listevents;
+
+						self.inprogress=false;
+
+
+				})
+				.error( function() {
+					
+					self.inprogress=false;
+					});
+
+	}
+	this.refresh();
+
+
+	this.getIndicators = function ( photo, askMainIndicator )
+	{
+		var list = [];
+		for (var i=0;i<photo.listindicators.length; i++)
+		{
+			var indicator = photo.listindicators[ i ];
+			if (indicator.ismainindicator === askMainIndicator)
+				list.push( indicator );
+		}
+		return list;
+	}
+		
+
+	// -----------------------------------------------------------------------------------------
+	//  										Properties
+	// -----------------------------------------------------------------------------------------
+	this.propsFirstName='';
+	this.saveProps = function() {
+		var self=this;
+		self.inprogress=true;
+
+		var param={ 'firstname': this.propsFirstName };
+		var json = encodeURI( angular.toJson( param, false));
+
+		// 7.6 : the server force a cache on all URL, so to bypass the cache, then create a different URL
+		var d = new Date();
+
+		$http.get( '?page=custompage_workers&action=saveprops&paramjson='+json +'&t='+d.getTime())
+				.success( function ( jsonResult ) {
+						console.log("history",jsonResult);
+						self.listevents		= jsonResult.listevents;
+						self.inprogress=false;
+
+						
+				})
+				.error( function() {
+					
+					});
+	}
+
+	
+
+
+	<!-- Manage the event -->
+	this.getListEvents = function ( listevents ) {
+		return $sce.trustAsHtml(  listevents );
+	}
+	<!-- Manage the Modal -->
+	this.isshowDialog=false;
+	this.openDialog = function()
+	{
+		this.isshowDialog=true;
+	};
+	this.closeDialog = function()
+	{
+		this.isshowDialog=false;
+	}
+
+});
+
+
+
+})();
--- src/src/org/bonitasoft/custompage/workers/EngineMonitoringAPI.java
+++ src/src/org/bonitasoft/custompage/workers/EngineMonitoringAPI.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,7 +27,9 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.bonitasoft.custompage.workers.Photo.WorkerPhoto;
-
+import org.bonitasoft.deepmonitoring.radar.Radar;
+import org.bonitasoft.deepmonitoring.radar.RadarFactory;
+import org.bonitasoft.deepmonitoring.radar.RadarPhoto;
 
 /* -------------------------------------------------------------------- */
 /*                                                                      */
@@ -38,192 +41,213 @@ public class EngineMonitoringAPI {
 
   final static Logger logger = Logger.getLogger(EngineMonitoringAPI.class.getName());
   public static String loggerLabel = "LongBoard ##";
-    
-   
-    
-    
-    public static Map<String,Object> getWorkersInfo()
-    {
-        
-        Photo photo = calculatePhoto();
-        
-        return photo.getMap();
-        
-    }
-    
-    private static Photo calculatePhoto()
-    {
-        Photo photo = new Photo();
-        photo.datePhoto = new Date();
 
-        // --------------------------- Thread
-        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
-        for (Thread th : threadSet )
-        {
-            // Bonita-Worker-1-10
-            if (th.getName().startsWith("Bonita-Worker"))
-            {
-                try
-                {
-                    int posId = th.getName().lastIndexOf("-");
-                    int workerId = Integer.valueOf(  th.getName().substring(posId+1));
-                    
-                    // get or create it
-                    WorkerPhoto workerPhoto = photo.createWorkerPhoto( workerId, th );
-                                      
-                }
-                catch(Exception e)
-                {
-                    
-                }
-            }
-            if (th.getName().startsWith("ConnectorExecutor"))
-            {
-                
-                // how do we know which worker is link to this connector?
-                int posId = th.getName().lastIndexOf("-");
-                int workerId = Integer.valueOf(  th.getName().substring(posId+1));
-                // get or create it
-                WorkerPhoto workerPhoto = photo.createWorkerPhoto( workerId, th );
-            }
-        }
-        // --------------------------- CPU and memory
-        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-         photo.cpuLoad = osBean.getSystemLoadAverage();
-        
-        
-        // now synthesis
-
-        
-        // number of item in the queue
-        String sqlRequest="SELECT count(f.id) FROM FLOWNODE_INSTANCE f ";
-        sqlRequest += " WHERE (f.STATE_EXECUTING = ? "; // true
-        sqlRequest += " OR f.STABLE = ? "; // false
-        sqlRequest += " OR f.TERMINAL = ? "; // true
-        sqlRequest += " OR f.STATECATEGORY = 'ABORTING' OR f.STATECATEGORY='CANCELLING')";
-        
-        Connection con = null;
-        PreparedStatement pstmt=null;
-        ResultSet rs = null;
-        try
-        {
-          con = getConnection();
-        pstmt = con.prepareStatement(sqlRequest);
-        pstmt.setBoolean(1,  true);
-        pstmt.setBoolean(2,  false);
-        pstmt.setBoolean(3,  true);
-        
-        rs = pstmt.executeQuery();
-        while (rs.next()) {
-            photo.workerQueueNumber = rs.getInt(1);
-        }
-        } catch (Exception e) {
-          final StringWriter sw = new StringWriter();
-          e.printStackTrace(new PrintWriter(sw));
-          logger.severe("During getAllProcessInstance : " + e.toString() + " at " + sw.toString());
-
-      } finally {
-          if (rs != null) {
-              try {
-                  rs.close();
-                  rs = null;
-              } catch (final SQLException localSQLException) {
-              }
-          }
-          if (pstmt != null) {
-              try {
-                  pstmt.close();
-                  pstmt = null;
-              } catch (final SQLException localSQLException) {
-              }
-          }
-          if (con != null) {
-              try {
-                  con.close();
-                  con = null;
-              } catch (final SQLException localSQLException1) {
-              }
-          }
-      }
-        
-        
-        return photo;
-    }
-    
-    private static void sortTheList(List<Map<String, Object>> listToSort, final String attributName) {
-
-      Collections.sort(listToSort, new Comparator<Map<String, Object>>() {
-
-          public int compare(final Map<String, Object> s1, final Map<String, Object> s2) {
-              
-              StringTokenizer st = new StringTokenizer(attributName,  ";");
-              while (st.hasMoreTokens())
-              {
-                  String token = st.nextToken();
-                  Object d1 = s1.get( token );
-                  Object d2 = s2.get( token );
-                  if (d1!=null && d2!=null)
-                  {
-                      int comparaison=0;
-                      if (d1 instanceof String)
-                          comparaison=((String) d1).compareTo( ((String)d2));
-                      if (d1 instanceof Integer)
-                          comparaison=((Integer) d1).compareTo( ((Integer)d2));
-                      if (d1 instanceof Long)
-                          comparaison=((Long) d1).compareTo( ((Long)d2));
-                      if (comparaison!=0)
-                          return comparaison;
-                  }
-                  // one is null, or both are null : continue
-              }
-              return 0;
-          }
-      });
-      ;
+  public static Map<String, Object> getListNameRadars() {
+    Map<String, Object> map = new HashMap<String, Object>();
+    RadarFactory radarFactory = RadarFactory.getinstance();
+    map.put("radars", radarFactory.getListRadar());
+    return map;
   }
 
-    
-    /* -------------------------------------------------------------------- */
-    /*                                                                      */
-    /* getConnection */
-    /*                                                                      */
-    /* -------------------------------------------------------------------- */
-    private static List<String> listDataSources = Arrays.asList("java:/comp/env/bonitaSequenceManagerDS",
-            "java:jboss/datasources/bonitaSequenceManagerDS");
+  /**
+   * get a static photo on all the radar
+   * Nota: some radar may need to be started to do the job: then the give what they can give.
+   * 
+   * @return
+   */
+  public static Map<String, Object> getAllPhoto() {
+    Map<String, Object> map = new HashMap<String, Object>();
+    long timeBegin = System.currentTimeMillis();
 
-    /**
-     * getConnection
-     * 
-     * @return
-     * @throws NamingException
-     * @throws SQLException
-     */
+    List<Map<String, Object>> listRadarsPhoto = new ArrayList<Map<String, Object>>();
+    RadarFactory radarFactory = RadarFactory.getinstance();
+    for (String radarName : radarFactory.getListRadar()) {
+      Radar radar = radarFactory.getRadar(radarName);
+      List<RadarPhoto> listRadarPhotos = radar.takePhoto();
 
-    public static Connection getConnection() throws SQLException {
-        // logger.info(loggerLabel+".getDataSourceConnection() start");
-
-        String msg = "";
-        List<String> listDatasourceToCheck = new ArrayList<String>();
-        for (String dataSourceString : listDataSources)
-            listDatasourceToCheck.add(dataSourceString);
-
-        for (String dataSourceString : listDatasourceToCheck) {
-            logger.info(loggerLabel + ".getDataSourceConnection() check[" + dataSourceString + "]");
-            try {
-                final Context ctx = new InitialContext();
-                final DataSource dataSource = (DataSource) ctx.lookup(dataSourceString);
-                logger.info(loggerLabel + ".getDataSourceConnection() [" + dataSourceString + "] isOk");
-                return dataSource.getConnection();
-
-            } catch (NamingException e) {
-                logger.info(
-                        loggerLabel + ".getDataSourceConnection() error[" + dataSourceString + "] : " + e.toString());
-                msg += "DataSource[" + dataSourceString + "] : error " + e.toString() + ";";
-            }
-        }
-        logger.severe(loggerLabel + ".getDataSourceConnection: Can't found a datasource : " + msg);
-        return null;
+      Map<String, Object> radarMap = radar.getMap();
+      List<Map<String, Object>> listPhotos = new ArrayList<Map<String, Object>>();
+      for (RadarPhoto photo : listRadarPhotos) {
+        listPhotos.add(photo.getMap());
+      }
+      radarMap.put("photos", listPhotos);
+      listRadarsPhoto.add( radarMap );
     }
-    
-    
+
+    map.put("radars", listRadarsPhoto);
+    map.put("timecollectms", System.currentTimeMillis() - timeBegin);
+    // SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+    map.put("datecollectms", timeBegin);
+
+    return map;
+
+  }
+
+  public static Map<String, Object> getWorkersInfo() {
+
+    Photo photo = calculatePhoto();
+
+    return photo.getMap();
+
+  }
+
+  private static Photo calculatePhoto() {
+    Photo photo = new Photo();
+    photo.datePhoto = new Date();
+
+    // --------------------------- Thread
+    Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+    for (Thread th : threadSet) {
+      // Bonita-Worker-1-10
+      if (th.getName().startsWith("Bonita-Worker")) {
+        try {
+          int posId = th.getName().lastIndexOf("-");
+          int workerId = Integer.valueOf(th.getName().substring(posId + 1));
+
+          // get or create it
+          WorkerPhoto workerPhoto = photo.createWorkerPhoto(workerId, th);
+
+        } catch (Exception e) {
+
+        }
+      }
+      if (th.getName().startsWith("ConnectorExecutor")) {
+
+        // how do we know which worker is link to this connector?
+        int posId = th.getName().lastIndexOf("-");
+        int workerId = Integer.valueOf(th.getName().substring(posId + 1));
+        // get or create it
+        WorkerPhoto workerPhoto = photo.createWorkerPhoto(workerId, th);
+      }
+    }
+    // --------------------------- CPU and memory
+    OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    photo.cpuLoad = osBean.getSystemLoadAverage();
+
+    // now synthesis
+
+    // number of item in the queue
+    String sqlRequest = "SELECT count(f.id) FROM FLOWNODE_INSTANCE f ";
+    sqlRequest += " WHERE (f.STATE_EXECUTING = ? "; // true
+    sqlRequest += " OR f.STABLE = ? "; // false
+    sqlRequest += " OR f.TERMINAL = ? "; // true
+    sqlRequest += " OR f.STATECATEGORY = 'ABORTING' OR f.STATECATEGORY='CANCELLING')";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sqlRequest);
+      pstmt.setBoolean(1, true);
+      pstmt.setBoolean(2, false);
+      pstmt.setBoolean(3, true);
+
+      rs = pstmt.executeQuery();
+      while (rs.next()) {
+        photo.workerQueueNumber = rs.getInt(1);
+      }
+    } catch (Exception e) {
+      final StringWriter sw = new StringWriter();
+      e.printStackTrace(new PrintWriter(sw));
+      logger.severe("During getAllProcessInstance : " + e.toString() + " at " + sw.toString());
+
+    } finally {
+      if (rs != null) {
+        try {
+          rs.close();
+          rs = null;
+        } catch (final SQLException localSQLException) {
+        }
+      }
+      if (pstmt != null) {
+        try {
+          pstmt.close();
+          pstmt = null;
+        } catch (final SQLException localSQLException) {
+        }
+      }
+      if (con != null) {
+        try {
+          con.close();
+          con = null;
+        } catch (final SQLException localSQLException1) {
+        }
+      }
+    }
+
+    return photo;
+  }
+
+  private static void sortTheList(List<Map<String, Object>> listToSort, final String attributName) {
+
+    Collections.sort(listToSort, new Comparator<Map<String, Object>>() {
+
+      public int compare(final Map<String, Object> s1, final Map<String, Object> s2) {
+
+        StringTokenizer st = new StringTokenizer(attributName, ";");
+        while (st.hasMoreTokens()) {
+          String token = st.nextToken();
+          Object d1 = s1.get(token);
+          Object d2 = s2.get(token);
+          if (d1 != null && d2 != null) {
+            int comparaison = 0;
+            if (d1 instanceof String)
+              comparaison = ((String) d1).compareTo(((String) d2));
+            if (d1 instanceof Integer)
+              comparaison = ((Integer) d1).compareTo(((Integer) d2));
+            if (d1 instanceof Long)
+              comparaison = ((Long) d1).compareTo(((Long) d2));
+            if (comparaison != 0)
+              return comparaison;
+          }
+          // one is null, or both are null : continue
+        }
+        return 0;
+      }
+    });;
+  }
+
+  /* -------------------------------------------------------------------- */
+  /*                                                                      */
+  /* getConnection */
+  /*                                                                      */
+  /* -------------------------------------------------------------------- */
+  private static List<String> listDataSources = Arrays.asList("java:/comp/env/bonitaSequenceManagerDS",
+      "java:jboss/datasources/bonitaSequenceManagerDS");
+
+  /**
+   * getConnection
+   * 
+   * @return
+   * @throws NamingException
+   * @throws SQLException
+   */
+
+  public static Connection getConnection() throws SQLException {
+    // logger.info(loggerLabel+".getDataSourceConnection() start");
+
+    String msg = "";
+    List<String> listDatasourceToCheck = new ArrayList<String>();
+    for (String dataSourceString : listDataSources)
+      listDatasourceToCheck.add(dataSourceString);
+
+    for (String dataSourceString : listDatasourceToCheck) {
+      logger.info(loggerLabel + ".getDataSourceConnection() check[" + dataSourceString + "]");
+      try {
+        final Context ctx = new InitialContext();
+        final DataSource dataSource = (DataSource) ctx.lookup(dataSourceString);
+        logger.info(loggerLabel + ".getDataSourceConnection() [" + dataSourceString + "] isOk");
+        return dataSource.getConnection();
+
+      } catch (NamingException e) {
+        logger.info(
+            loggerLabel + ".getDataSourceConnection() error[" + dataSourceString + "] : " + e.toString());
+        msg += "DataSource[" + dataSourceString + "] : error " + e.toString() + ";";
+      }
+    }
+    logger.severe(loggerLabel + ".getDataSourceConnection: Can't found a datasource : " + msg);
+    return null;
+  }
+
 }
